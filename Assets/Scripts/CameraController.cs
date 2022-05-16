c8fa6714f6f5eb7d7a793371f19b10d79d15a734
jaximoto <62913915+jaximoto@users.noreply.github.com>
2022-05-15 21:02:57 -0700

    Added camera following player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,11 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private float currentposX;
-    private Vector2 velocity = Vector2.zero;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +20,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.position = Vector2.SmoothDamp(transform.position, new Vector2(currentposX, transform.position.y), ref velocity, speed * Time.deltaTime);
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentposX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
     public void NewRoom(Transform _newroom)
     {
-
+        currentposX = _newroom.position.x;
     }
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private Transform previousRoom;
-    [SerializeField] private Transform nexttRoom;
-
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
     // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            if(collision.transform.position.x < transform.position.x)
+            {
+                cam.NewRoom(nextRoom);
+            }
+            else
+            {
+                cam.NewRoom(previousRoom);
+            }
+        }
     }
 }
